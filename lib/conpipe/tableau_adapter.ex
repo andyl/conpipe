518a520ef6d4ea6f31979f920e65ebcd78d0b266
andyl <andy@r210.com>
2024-10-11 17:52:32 -0700

    Tweak documentation

--- lib/conpipe/tableau_adapter.ex
+++ lib/conpipe/tableau_adapter.ex
@@ -12,12 +12,12 @@ defmodule Conpipe.TableauAdapter do
       config :tableau, :config,
         url: "http://localhost:4999",
         converters: [
-          md: <ConverterModule>
+          md: #{inspect(__MODULE__)}
         ]
       ```
 
-      This function returns just the converted text. It should not be used in a
-      pipeline.
+      The `convert/4` function returns just the converted text. It should not
+      be used in a pipeline.
       """
       def convert(filepath, front_matter, body, opts) do
         assigns =
